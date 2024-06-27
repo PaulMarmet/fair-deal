43da0e0d8c7697f7e7a89703af45d9529bf59cc1
PaulMarmet <paulmarmet1002@gmail.com>
2024-06-27 18:27:38 -0400

    The Foundation has been built, now, time for the real changes...

---
+++ src/main/java/net/pm/FairDeal.java
@@ -0,0 +1,16 @@
+package net.pm;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FairDeal implements ModInitializer {
+	public static final String MOD_ID = "fair_deal";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
